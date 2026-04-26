1b10ca5a2a48620deca5e04f6583d763128a4ac8
Jan Burkhardt <burkhardt.jan.august@gmail.com>
2026-04-26 23:22:42 +0200

    connected database to flask-app

--- app.py
+++ app.py
@@ -1,6 +1,29 @@
+import os
 from flask import Flask
+from data_manager import DataManager
+from models import db, Movie
 
 app = Flask(__name__)
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+data_dir = os.path.join(basedir, 'data')
+if not os.path.exists(data_dir):
+    os.makedirs(data_dir)
+app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(data_dir, 'movies.db')}"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)  # Link the database and the app. This is the reason you need to import db from models
+
+data_manager = DataManager()  # Create an object of your DataManager class
+
+
+@app.route('/')
+def home():
+    return "Welcome to MoviWeb App!"
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    with app.app_context():
+        db.create_all()
+
+    app.run()
