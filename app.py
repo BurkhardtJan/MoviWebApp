482b24e05fe0b501c3a193ce49160a76568ddabb
Jan Burkhardt <burkhardt.jan.august@gmail.com>
2026-04-27 19:18:24 +0200

    added error handling

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, abort
 from data_manager import DataManager
 from models import db, Movie
 
@@ -34,7 +34,11 @@ def list_users():
 def create_user():
     """Adds a new user to the database."""
     new_user = request.form.get("new_user")
-    data_manager.create_user(new_user)
+    try:
+        data_manager.create_user(new_user)
+    except Exception as e:
+        print("An error occurred: ", str(e), 500)
+        return "An error occurred: " + str(e), 500
     return redirect(url_for('index'))
 
 
@@ -42,6 +46,8 @@ def create_user():
 def get_movies(user_id):
     """Shows a list of movies for a user."""
     user = data_manager.get_user_by_id(user_id)
+    if user is None:
+        abort(404)
     return render_template("movies.html", movies=data_manager.get_movies(user_id), user=user)
 
 
@@ -50,7 +56,11 @@ def add_movie(user_id):
     """Adds a new movie for user to the database."""
     movie_title = request.form.get("movie_title")
     movie_year = request.form.get("movie_year")
-    data_manager.add_movie(movie_title, user_id)
+    try:
+        data_manager.add_movie(movie_title, user_id)
+    except Exception as e:
+        print("An error occurred: ", str(e), 500)
+        return "An error occurred: " + str(e), 500
     return redirect(url_for('get_movies', movies=data_manager.get_movies(user_id), user_id=user_id))
 
 
@@ -58,15 +68,28 @@ def add_movie(user_id):
 def update_movie(user_id, movie_id):
     """Updates a movie title manually for user to the database."""
     new_title = request.form.get("new_title")
-    data_manager.update_movie(movie_id, new_title)
+    try:
+        data_manager.update_movie(movie_id, new_title)
+    except Exception as e:
+        print("An error occurred: ", str(e), 500)
+        return "An error occurred: " + str(e), 500
     return redirect(url_for('get_movies', movies=data_manager.get_movies(user_id), user_id=user_id))
 
 
 @app.route('/users/<int:user_id>/movies/<int:movie_id>/delete', methods=['POST'])
 def delete_movie(user_id, movie_id):
     """Deletes a movie for user to the database."""
-    data_manager.delete_movie(movie_id)
+    try:
+        data_manager.delete_movie(movie_id)
+    except Exception as e:
+        print("An error occurred: ", str(e), 500)
+        return "An error occurred: " + str(e), 500
     return redirect(url_for('get_movies', movies=data_manager.get_movies(user_id), user_id=user_id))
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 
 if __name__ == '__main__':
